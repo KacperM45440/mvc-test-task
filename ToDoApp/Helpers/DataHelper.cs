330594f28601b7b2726df41d3d9ad521a5b82f6a
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-11-09 11:56:17 +0100

    Task 6

---
+++ ToDoApp/Helpers/DataHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Helpers
+{
+    public class DataHelper
+    {
+        public static List<ComboBoxItem> GetYears(DateTime startDate)
+        {
+            List<ComboBoxItem> yearList = new List<ComboBoxItem>();
+            int startYear = startDate.Year - 5;
+            int endYear = startDate.Year + 5;
+            int id = 1;
+
+            for (int i = startYear; i <= endYear; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem()
+                {
+                    ID = id,
+                    Name = i.ToString()
+                };
+
+                yearList.Add(item);
+                id++;
+            }
+
+            return yearList;
+        }
+    }
+}
